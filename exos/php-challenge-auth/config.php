643a9ce56db232e060a04b9f939277ac9a8386fd
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-06-17 15:02:06 +0200

    ok

---
+++ exos/php-challenge-auth/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=exosSQL;charset=utf8';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connexion échouée : ' . $e->getMessage());
+}
+
